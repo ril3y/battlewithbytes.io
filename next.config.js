ff786759f997bba23db0912a93389b225433c5ae
Riley <rileyporter@gmail.com>
2025-05-09 08:23:29 -0400

    Missing config file for cicd build.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Added for static site generation
   reactStrictMode: false,
   images: {
     remotePatterns: [
