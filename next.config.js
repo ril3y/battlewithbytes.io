3fc1b0291ffd7370f565fb580ef452eecbe6171a
Riley <rileyporter@gmail.com>
2025-05-09 08:30:26 -0400

    fixed images bug

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Added for static site generation
+  basePath: '/battlewithbytes.io', // Added for GitHub Pages deployment
+  assetPrefix: '/battlewithbytes.io/', // Often used with basePath for assets
   reactStrictMode: false,
   images: {
+    unoptimized: true, // Added for static export compatibility
     remotePatterns: [
       {
         protocol: 'https',
