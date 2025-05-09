62a88d807575df01a153bae06e4e40e450c4decf
Riley <rileyporter@gmail.com>
2025-05-09 09:06:05 -0400

    added image widget, fixed some issues with images.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,11 @@
 import RadixTabs from './src/components/RadixTabs';
+import ImageWidget from './src/components/ImageWidget';
 import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     RadixTabs,
+    ImageWidget,
     ...components,
   };
 }
