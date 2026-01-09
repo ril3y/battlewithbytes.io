2e2ef052c177b9a6bf97ce6f43e2cf6bbe98dbd4
Riley <rileyporter@gmail.com>
2026-01-09 15:16:31 -0500

    fix(utils): add missing eslint.config.mjs for ESLint v9

---
+++ packages/utils/eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+const eslintConfig = [
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "prefer-const": "warn",
+    },
+  },
+];
+
+export default eslintConfig;
